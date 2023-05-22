8e560d15cb4fe8c79ef2d24b3427050bdb6ac61d
helloitsjoe <jboyle617@gmail.com>
2023-05-22 13:10:06 -0400

    finish off lookup domain

--- python/response.py
+++ python/response.py
@@ -1,6 +1,7 @@
 from main import build_query, DNSQuestion, DNSHeader
 from dataclasses import dataclass
 from io import BytesIO
+from typing import List
 import struct
 import socket
 
@@ -13,6 +14,14 @@ class DNSRecord:
     class_: int
     ttl: int
     data: bytes
+
+@dataclass
+class DNSPacket:
+    header: DNSHeader
+    questions: List[DNSQuestion]
+    answers: List[DNSRecord]
+    authorities: List[DNSRecord]
+    additionals: List[DNSRecord]
 
 def parse_header(reader):
     items = struct.unpack("!HHHHHH", reader.read(12))
@@ -33,6 +42,19 @@ def parse_record(reader):
     type_, class_, ttl, data_len = struct.unpack("!HHIH", data)
     data = reader.read(data_len)
     return DNSRecord(name, type_, class_, ttl, data)
+
+def parse_dns_packet(data):
+    reader = BytesIO(data)
+    header = parse_header(reader)
+    questions = [parse_question(reader) for _ in range(header.num_questions)]
+    answers = [parse_record(reader) for _ in range(header.num_answers)]
+    authorities = [parse_record(reader) for _ in range(header.num_authorities)]
+    additionals = [parse_record(reader) for _ in range(header.num_additionals)]
+
+    return DNSPacket(header, questions, answers, authorities, additionals)
+
+def ip_to_string(ip):
+    return ".".join([str(x) for x in ip])
 
 # def decode_name_simple(reader):
 #     parts = []
@@ -59,20 +81,20 @@ def decode_compressed_name(length, reader):
     reader.seek(current_pos)
     return result
 
-query = build_query("www.example.com", TYPE_A)
+def lookup_domain(domain_name, type=TYPE_A):
+    print(type)
+    query = build_query(domain_name, type)
+    # create a UDP socket
+    # `socket.AF_INET` means that we're connecting to the internet
+    # (as opposed to a Unix domain socket `AF_UNIX` for example)
+    # `socket.SOCK_DGRAM` means "UDP"
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(query, ("8.8.8.8", 53))
 
-# create a UDP socket
-# `socket.AF_INET` means that we're connecting to the internet
-# (as opposed to a Unix domain socket `AF_UNIX` for example)
-# `socket.SOCK_DGRAM` means "UDP"
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Get the response
+    data, _ = sock.recvfrom(1024)
+    response = parse_dns_packet(data)
+    return ip_to_string(response.answers[0].data)
 
-sock.sendto(query, ("8.8.8.8", 53))
-
-response, _ = sock.recvfrom(1024)
-
-reader = BytesIO(response)
-header = parse_header(reader)
-question = parse_question(reader)
-record = parse_record(reader)
-print(record)
+print(lookup_domain("google.com"))
+print(lookup_domain("www.runtimerundown.com", 5))
