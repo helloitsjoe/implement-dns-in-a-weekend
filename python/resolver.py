de62ce39dbaae3b9b891222620efca5349fc8e0d
helloitsjoe <jboyle617@gmail.com>
2023-05-27 08:32:26 -0400

    resolver

--- python/resolver.py
+++ python/resolver.py
@@ -1,5 +1,5 @@
 from main import DNSRecord, DNSPacket, DNSQuestion, DNSHeader, header_to_bytes, question_to_bytes, encode_dns_name, build_query
-from response import decode_name, parse_header, parse_question, parse_dns_packet, ip_to_string, TYPE_A
+from response import decode_name, parse_header, parse_question, parse_dns_packet, ip_to_string, TYPE_A, TYPE_NS
 import socket
 
 def send_query(ip_address, domain_name, record_type):
@@ -10,6 +10,35 @@ def send_query(ip_address, domain_name, record_type):
     data, _ = sock.recvfrom(1024)
     return parse_dns_packet(data)
 
-response = send_query("198.41.0.4", "runtimerundown.com", TYPE_A)
-print(response.answers)
-print(response.authorities)
+def get_answer(packet):
+    for x in packet.answers:
+        if x.type_ == TYPE_A:
+            return x.data
+
+def get_nameserver(packet):
+    for x in packet.authorities:
+        if x.type_ == TYPE_NS:
+            return x.data.decode('utf-8')
+
+def get_nameserver_ip(packet):
+    for x in packet.additionals:
+        if x.type_ == TYPE_A:
+            return x.data
+
+def resolve(domain_name, record_type):
+    nameserver = "198.41.0.4"
+    while True:
+        print(f"Querying {nameserver} for {domain_name}")
+        response = send_query(nameserver, domain_name, record_type)
+        if ip := get_answer(response):
+            return ip
+        elif nsIP := get_nameserver_ip(response):
+            nameserver = nsIP
+        elif ns_domain := get_nameserver(response):
+            nameserver = resolve(ns_domain, TYPE_A)
+        else:
+            raise Exception("Something went wrong")
+
+
+print(resolve("twitter.com", TYPE_A))
+
